468efefb8d0e07a7eeb5fc2000fdafbb5f5c844d
jby9725 <jby9725@gmail.com>
2024-08-06 16:46:31 +0900

    리스트 테이블로 표현, 게시글 삭제 기능 구현

---
+++ DB.sql
@@ -0,0 +1,44 @@
+USE `ArticleManager`;
+
+SHOW TABLES;
+
+SELECT * FROM article;
+
+SELECT * FROM `member`;
+
+DROP TABLE article;
+
+DROP TABLE `member`;
+
+CREATE TABLE article (
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `regDate` DATETIME NOT NULL,
+    `updateDate` DATETIME NOT NULL,
+    `author` INT UNSIGNED NOT NULL,
+    `title` VARCHAR(50) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+-- `member` table 생성
+CREATE TABLE `member` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    `regDate`	DATETIME NOT NULL,
+    `userId` VARCHAR(100) NOT NULL UNIQUE,
+    `password` VARCHAR(100) NOT NULL,
+    `nickname` VARCHAR(100) NOT NULL
+);
+
+INSERT INTO `member` (`regDate`, `userId`, `password`, `nickname`)
+VALUES
+    (NOW(), 'test01', 'test01', '홍길동'),
+    (NOW(), 'test02', 'test02', '홍길순'),
+    (NOW(), 'test03', 'test03', '임꺽정');
+
+-- 문자열 붙이기 + 랜덤 수 출력
+INSERT INTO article
+SET regDate = NOW(),
+    updateDate = NOW(),
+    author = 1,
+    title = CONCAT('제목', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),
+    `body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2));
---
+++ src/main/java/servlet/ArticleListServlet.java
@@ -0,0 +1,76 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import util.DBUtil;
+import util.SecSql;
+
+//@WebServlet("/article/list")
+public class ArticleListServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		// DB 연결
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스 x");
+			e.printStackTrace();
+		}
+
+		System.out.println("db 드라이버 연결 완료");
+
+		// String url = "jdbc:mysql://localhost:3306/ArticleManager";
+		String url = "jdbc:mysql://127.0.0.1:3306/ArticleManager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";		
+		System.out.println(url);
+		
+		String user = "root";
+		String password = "";
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			response.getWriter().append("연결 성공!");
+//			DBUtil dbUtil = new DBUtil(request, response);
+
+//			String sql = "SELECT * FROM article";
+//			String sql = "SELECT * FROM article ORDER BY id DESC";
+
+			SecSql sql = SecSql.from("SELECT *");
+			sql.append("FROM article");
+			sql.append("ORDER BY id DESC");
+			
+			List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
+//			response.getWriter().append(articleRows.toString());
+			
+			request.setAttribute("articleRows", articleRows);
+			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
+
+		} catch (SQLException e) {
+			System.out.println("에러 1-2 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
