2815d649f1fd5a50c17c071b5a5e709ebb5dd27e
jby9725 <jby9725@gmail.com>
2024-08-08 14:25:06 +0900

    jspf 추가, 디스패처 서블릿 도입(article/list만), 프로그램 입구 감소(list의 경우)

---
+++ src/main/java/servlet/DispatcherServlet.java
@@ -0,0 +1,101 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+import controller.ArticleController;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/s/*")
+public class DispatcherServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		// DB 연결
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스 x");
+			e.printStackTrace();
+		}
+		String url = "jdbc:mysql://127.0.0.1:3306/ArticleManager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+
+		String user = "root";
+		String password = "";
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+
+			boolean isLogined = false;
+			int loginedMemberId = -1;
+			Map<String, Object> loginedMember = null;
+
+			HttpSession session = request.getSession();
+
+			if (session.getAttribute("loginedMemberId") != null) {
+				isLogined = true;
+				loginedMemberId = (int) session.getAttribute("loginedMemberId");
+				loginedMember = (Map<String, Object>) session.getAttribute("loginedMember");
+			}
+			request.setAttribute("isLogined", isLogined);
+			request.setAttribute("loginedMemberId", loginedMemberId);
+			request.setAttribute("loginedMember", loginedMember);
+
+			String requestUri = request.getRequestURI();
+
+			System.out.println(requestUri);
+
+			String[] reqUriBits = requestUri.split("/");
+			// ~~/s/article/list
+//			System.out.println(reqUriBits[0]);
+//			System.out.println(reqUriBits[1]);
+//			System.out.println(reqUriBits[2]);
+//			System.out.println(reqUriBits[3]);
+//			System.out.println(reqUriBits[4]);
+
+			if (reqUriBits.length < 5) {
+				response.getWriter().append(String.format("<script>alert('올바른 요청이 아닙니다.'); </script>"));
+				return;
+			}
+
+			String controllerName = reqUriBits[3];
+			String actionMethodName = reqUriBits[4];
+
+			if (controllerName.equals("article")) {
+				ArticleController articleController = new ArticleController(request, response, conn);
+
+				if (actionMethodName.equals("list")) {
+					articleController.showList();
+				}
+
+			}
+		} catch (SQLException e) {
+			System.out.println("에러 1 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- src/main/webapp/jsp/home/main.jsp
+++ src/main/webapp/jsp/home/main.jsp
@@ -18,39 +18,14 @@ Map<String, Object> loginedMember = (Map<String, Object>) request.getAttribute("
 <body>
 	<h1>MAIN</h1>
 	
-	<div><%=isLogined%></div>
-	<div><%=loginedMemberId%>번 회원 로그인 중입니다.</div>
-	<div><%=loginedMember%></div>
-
-	<%
-	if (isLogined) {
-	%>
-	<div>
-		<ul>
-		<li><a href="../member/doLogout">로그아웃</a></li>
-		<li><a href="../article/write">글쓰기</a></li>
-		</ul>
-	</div>
-	<%
-	}
-	%>
-
-	<%
-	if (!isLogined) {
-	%>
-	<div>글쓰기는 회원만 가능합니다.</div>
-	<div>
-		<a href="../member/login">로그인</a>
-	</div>
-	<div>
-		<a href="../member/join">회원가입</a>
-	</div>
-	<%
-	}
-	%>
+	<%@ include file="../part/top_bar.jspf"%>
 	
 	<ul>
+
 		<li><a href="../article/list">게시물 리스트로 이동</a></li>
+		<li><a href="../article/write">글쓰기</a></li>
+		<li><a href="../member/join">회원가입</a></li>
 	</ul>
+	
 </body>
 </html>
