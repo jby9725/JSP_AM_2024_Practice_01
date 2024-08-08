ec951ed589c363759198f2ac9022bb5b7dda03e4
jby9725 <jby9725@gmail.com>
2024-08-08 14:33:09 +0900

    Serivice 구조 도입

--- src/main/java/service/ArticleService.java
+++ src/main/java/service/ArticleService.java
@@ -1,5 +1,38 @@
 package service;
 
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+
+import jakarta.servlet.http.HttpSession;
+import util.DBUtil;
+import util.SecSql;
+
 public class ArticleService {
+
+	private Connection conn;
+
+	public ArticleService(Connection conn) {
+		this.conn = conn;
+	}
+	
+	public int getTotalCnt() {
+		SecSql sql = SecSql.from("SELECT COUNT(*) AS cnt");
+		sql.append("FROM article");
+
+		return DBUtil.selectRowIntValue(conn, sql);
+	}
+
+	public List<Map<String, Object>> getForPrintArticles(int limitFrom, int itemsInAPage) {
+
+		SecSql sql = SecSql.from("SELECT A.*, M.nickname");
+		sql.append("FROM article AS A");
+		sql.append("INNER JOIN `member` AS M");
+		sql.append("ON A.author = M.id");
+		sql.append("ORDER BY id DESC");
+		sql.append("LIMIT ?, ?;", limitFrom, itemsInAPage);
+
+		return DBUtil.selectRows(conn, sql);
+	}
 
 }
